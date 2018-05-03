e9ce4ccc1115e1f664d57536c1f7f9f3b0f8cbf7
sos171805vgm <vicengmestre@gmail.com>
2018-05-03 17:37:13 +0000

    v

---
+++ test/T01-loadDataWorld.js
@@ -0,0 +1,19 @@
+//un test puede estar compuesto por varias comprobaciones.
+describe("Data is loaded", function(){ //compruebo que los datos se cargan
+    //por cada comprobación ponemos it.
+    it("tiene que mostrar mas de 6",function(){//should show 5 best
+     
+                                //coloco la dirección del cloud9
+        browser.get('https://sos1718vgm-sos171805vgm.c9users.io/#!/world-stats'). //mepermite acceder al navegador donde esta el desplegado
+        then(function(){ //cuando cargue la pagina, vas a comrpobar si hay recursos
+            element //localizo una parte de la página..all te da todos. que cumplan una condicion (ngrepeat de la vista)
+            .all(by.repeater('world in worldStats'))
+            .then(function(worldStats){//aqui me devuelve un objeto worldStats que es un array con todos los recursos
+             console.log("porque sale vacio??");
+             console.log(worldStats);
+                expect(worldStats.length).toBeGreaterThan(6);
+            });        
+            
+        });
+    });
+});
