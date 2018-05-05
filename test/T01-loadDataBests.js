ab921e8ab33a769afa2103aa57aef0aa8b44e989
sos171805jpr <b-cube@hotmail.es>
2018-05-05 14:54:59 +0000

    limpio

--- test/T01-loadDataBests.js
+++ test/T01-loadDataBests.js
@@ -1,18 +1,22 @@
 /*global expect browser element by*/
+var fs = require("fs");
+var path = require("path");
 describe("Data is loaded", function(){
     it("should show 5 best",function(){
         browser.get('https://sos171805jpr-sos171805jpr.c9users.io/#!/best-stats').
         then(function(){
-            element
-            .all(by.repeater('best in bests'))
-            .then(function(bests){
-                console.log(bests);
-                expect(bests.length).toEqual(5);
+            element.all(by.repeater('best in bests')).then(function(bests){
+            console.log(bests);
+            browser
+                .takeScreenshot()
+                .then(function (png){
+                    var stream = fs.createWriteStream(path.join(process.cwd(),'test','output','T01.png'));
+                    stream.write(new Buffer(png, 'base64'));
+                    stream.end();
+                });
+            expect(bests.length).toEqual(5);
             });        
-            
         });
-        
-        
     });
 });
 
