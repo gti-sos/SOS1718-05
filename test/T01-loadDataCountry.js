f92248620713bbe575b7c88b88db3a7a61b107f4
sos171805mbg <manubare@hotmail.com>
2018-05-06 11:18:23 +0000

    manu

--- test/T01-loadDataCountry.js
+++ test/T01-loadDataCountry.js
@@ -1,7 +1,22 @@
 /*global expect browser element by*/
-describe("Data is loaded", function(){
-    it("should show 10 items",function(){
-        expect(1).toEqual(1);
+var fs = require("fs");
+var path = require("path");
+describe("Data is loaded", function() {
+    it("should show 5 country stats", function() {
+        browser.get('https://sos171805-mbg1-sos171805mbg.c9users.io/#!/country-stats').
+        then(function() {
+            element.all(by.repeater('country in countryStats')).then(function(countryStats) {
+                console.log(countryStats);
+                browser
+                    .takeScreenshot()
+                    .then(function(png) {
+                        var stream = fs.createWriteStream(path.join(process.cwd(), 'test', 'output', 'T01-country.png'));
+                        stream.write(new Buffer(png, 'base64'));
+                        stream.end();
+                    });
+                expect(countryStats.length).toEqual(5);
+            });
+        });
     });
 });
 
--- test/T02-addDataCountry.js
+++ test/T02-addDataCountry.js
@@ -1,30 +1,36 @@
-describe("Add item", function(){
-    it("should add a new item",function(){
-        browser
-            .get('https://sos171805-mbg1-sos171805mbg.c9users.io/#!/country-stats').
-            then(function(){
-            
-            element
-                .all(by.repeater('country in countryStats')).then(function(initialdata){
-                browser.driver.sleep(2000); //duermo durante unos segundos para asegurarme que se ha cargado todo correctamente
+/*global expect browser element by*/
+describe("Add country", function(){
+    it("should add a new country item",function(){
+        browser.get('https://sos171805-mbg1-sos171805mbg.c9users.io').
+        then(function(){
+            element.all(by.buttonText("Country stats api")).last().click().then(function(){
                 
-                element(by.model('newCountry.country')).sendKeys("Matalascañas");
-                element(by.model('newCountry.rank')).sendKeys("2020");
-                element(by.model('newCountry.title')).sendKeys("venao");
-                element(by.model('newCountry.year')).sendKeys("vicente");
-                element(by.model('newCountry.certification')).sendKeys("50cent");
+                element.all(by.repeater('best in bests')).then(function(InitialBests){
+                browser.driver.sleep(2000);
                 
-                //localizo el boton que tiene ADD y le digo click
-                element(by.buttonText('Add')).click().then(function(){ //verificamos que tenemos los recursos iniciales mas 1.
-                    element                                             //verificamos el post.
-                        .all(by.repeater('country in countryStats')).then(function(countryStats){
-                         expect(countryStats.length).toEqual(initialdata.length+1); 
+                element(by.model('newBest.country')).sendKeys("Spain");
+                element(by.model('newBest.year')).sendKeys("2018");
+                element(by.model('newBest.album')).sendKeys("hola");
+                element(by.model('newBest.radio')).sendKeys("que");
+                element(by.model('newBest.song')).sendKeys("tal");
+                
+                element(by.buttonText('Add')).click().then(function(){
+                    element.all(by.repeater('best in bests')).then(function(bests){
+                       expect(bests.length).toEqual(InitialBests.length+1); 
                     });
                 });
                 
                 
-            });        
+            }); 
+                
+                
+                
+                
+            })       
             
         });    
+      
+        
+        
     });
 });
