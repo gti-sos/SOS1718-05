e9d6464fdfc35c9975f3b5503701fca2522a5d78
sos171805vgm <vicengmestre@gmail.com>
2018-05-07 08:53:10 +0000

    c

--- public/world-stats-api/listWorldCtrl.js
+++ public/world-stats-api/listWorldCtrl.js
@@ -157,8 +157,7 @@ angular.module("MusicApp").controller("ListWorldCtrl",["$scope","$http",function
                 });
             }
             get();
-            
-            
+            //
             
              //LOAD INITIAL DATA
     $scope.loadInitialData = function(){
