4c42fd7d9f42b75b98b671333a1287cc25b28061
sos171805mbg <manubare@hotmail.com>
2018-05-24 22:46:00 +0000

    update automatizacion manu

--- test/T01-loadDataCountry.js
+++ test/T01-loadDataCountry.js
@@ -1,6 +1,7 @@
 /*global expect browser element by*/
 var fs = require("fs");
 var path = require("path");
+var config = require('./configCountry')
 describe("Data is loaded", function() {
     it("should show initial countryStats", function() {
         browser.get('https://sos1718-05.herokuapp.com/#!/country-stats').
@@ -13,7 +14,7 @@ describe("Data is loaded", function() {
                         var stream = fs.createWriteStream(path.join(process.cwd(), 'test', 'output', 'T01-country.png'));
                         stream.write(new Buffer(png, 'base64'));
                         stream.end();
-                    });
+                    }); 
                 expect(countryStats.length).toBeGreaterThan(0);
             });
         });
