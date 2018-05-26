e4e890fd7b050d16581322993c7129beeb791b89
sos171805jpr <b-cube@hotmail.es>
2018-05-26 16:11:05 +0000

    pruebas back-end

--- test/config.js
+++ test/config.js
@@ -1,10 +1,7 @@
 exports.config = {
     seleniumAddress: "http://localhost:8910",//Direccion
-<<<<<<< HEAD
-    specs:["T01-loadDataBests.js","T02-addDataBests.js","T01-loadDataWorld.js","T02-addDataWorld.js","T01-loadDataCountry.js","T02-addDataCountry.js"],//Tests
-=======
-    specs:["T01-loadDataBests.js","T02-addDataBests.js","T01-loadDataWorld.js", "T02-addDataWorld.js","T01-loadDataCountry.js","T02-addDataCountry.js","T00-APICountry"],//Tests
->>>>>>> 9853de3ba670b55ea163c59ab013567f85e9f843
+    specs:["T00-APIBest.js","T01-loadDataBests.js","T02-addDataBests.js","T01-loadDataWorld.js", "T02-addDataWorld.js","T01-loadDataCountry.js","T02-addDataCountry.js","T00-APICountry"],//Tests
+    
     capabilities:{
         "browserName": "phantomjs"//navegador
     }
