61c5d36e846a96689113a515ec2b9c44fe57cc54
sos171805jpr <b-cube@hotmail.es>
2018-05-27 18:55:17 +0000

    grupal

--- public/grupal-analytic/highchartsGrupalCtrl.js
+++ public/grupal-analytic/highchartsGrupalCtrl.js
@@ -4,8 +4,65 @@ angular.module("MusicApp").controller("highchartsGrupalCtrl", ["$scope", "$http"
     $http.get("api/v1/best-stats").then(function(best) {
         $http.get("api/v1/world-stats").then(function(world) {
             $http.get("api/v1/country-stats").then(function(country) {
+                console.log(best.data);
+                console.log(world.data);
+                console.log(country.data);
                 
-                
+            var arr = [];   
+            best.data.forEach(f=>{
+            var arr2 = [];
+            
+            Object.keys(f).forEach(p=>{
+                if(p == "year"){
+                    arr2.push(f[p]);
+                    arr2.push(f[p]);
+                }else if(p == "song"){
+                    arr2.unshift(f[p]);
+                }
+                    
+                    
+                })
+            
+            
+            arr.push(arr2);
+        })
+        
+         world.data.forEach(f=>{
+            var arr2 = [];
+            
+            Object.keys(f).forEach(p=>{
+                if(p == "year"){
+                    arr2.push(f[p]);
+                    arr2.push(f[p]);
+                }else if(p == "album"){
+                    arr2.unshift(f[p]);
+                }
+                    
+                    
+                })
+            
+            
+            arr.push(arr2);
+        })
+        
+         country.data.forEach(f=>{
+            var arr2 = [];
+            
+            Object.keys(f).forEach(p=>{
+                if(p == "year"){
+                    arr2.push(f[p]);
+                    arr2.push(f[p]);
+                }else if(p == "title"){
+                    arr2.push(f[p]);
+                }
+                    
+                    
+                })
+            
+            
+            arr.push(arr2);
+        })
+         console.log(arr);       
                 
     Highcharts.chart('container', {
 
@@ -34,34 +91,14 @@ angular.module("MusicApp").controller("highchartsGrupalCtrl", ["$scope", "$http"
 
     series: [{
         name: 'Labor Costs',
-        data: [
-            ['Norway', 50.2, 335504],
-            ['Denmark', 42, 277339],
-            ['Belgium', 39.2, 421611],
-            ['Sweden', 38, 462057],
-            ['France', 35.6, 2228857],
-            ['Netherlands', 34.3, 702641],
-            ['Finland', 33.2, 215615],
-            ['Germany', 33.0, 3144050],
-            ['Austria', 32.7, 349344],
-            ['Ireland', 30.4, 275567],
-            ['Italy', 27.8, 1672438],
-            ['United Kingdom', 26.7, 2366911],
-            ['Spain', 21.3, 1113851],
-            ['Greece', 14.2, 175887],
-            ['Portugal', 13.7, 184933],
-            ['Czech Republic', 10.2, 176564],
-            ['Poland', 8.6, 424269],
-            ['Romania', 5.5, 169578]
-
-        ],
+        data:arr,
         dataLabels: {
             enabled: true,
-            format: '€{point.y:.0f}'
+            format: '{point.y:.0f}'
         },
         tooltip: {
-            pointFormat: 'Labor Costs: <b>€ {point.y}/h</b><br>' +
-                'GDP: <b>€ {point.z} million</b><br>'
+            pointFormat: 'Labor Costs: <b> {point.y}/h</b><br>' +
+                'GDP: <b> {point.z} million</b><br>'
         },
         colorByPoint: true
     }]
